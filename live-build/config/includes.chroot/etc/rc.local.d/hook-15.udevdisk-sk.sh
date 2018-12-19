8491ffbd69b40d1c71a3e0de09f3a006a35781e6
Marco Buratto <m.buratto@lumit.it>
2018-12-19 12:14:49 +0100

    Packages refactoring

--- live-build/config/includes.chroot/etc/rc.local.d/hook-15.udevdisk-sk.sh
+++ live-build/config/includes.chroot/etc/rc.local.d/hook-15.udevdisk-sk.sh
@@ -4,7 +4,7 @@
 # Halt system immediately if the boot USB (Secure-K USB) is removed.
 DEV=$(mount | grep persistence | grep mapper | cut -d " " -f 1 | cut -d "/" -f 4)
 if [ "$DEV" != "" ]; then
-    echo "KERNEL==\"${DEV:0:3}3\",ENV{UDISKS_IGNORE}=\"1\"" > /etc/udev/rules.d/99-hide-disk.rules 
+    echo "KERNEL==\"${DEV:0:3}2\",ENV{UDISKS_IGNORE}=\"1\"" > /etc/udev/rules.d/99-hide-disk.rules 
     echo "KERNEL==\"${DEV:0:3}5\",ENV{UDISKS_IGNORE}=\"1\"" >> /etc/udev/rules.d/99-hide-disk.rules 
     echo "KERNEL==\"${DEV:0:3}\",ACTION==\"remove\",SUBSYSTEM==\"block\",RUN+=\"/usr/bin/sdmem -ll\",RUN+=\"/bin/chvt 5\",RUN+=\"/sbin/halt -fpn\"" > /etc/udev/rules.d/99-halt-on-sk-disconnection.rules
 fi
